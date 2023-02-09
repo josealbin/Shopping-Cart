99ff80a74d42d30efac5d5c9e67ec964e3eeb422
Albin Jose <jose.albin@outlook.com>
2023-02-09 16:53:31 +1000

    shopping-cart initial ver 0.0.1

---
+++ helpers/product-helpers.js
@@ -0,0 +1,18 @@
+var db = require('../config/connection')
+var collection=require('../config/collections')
+
+module.exports = {
+    addProduct:(product,callback)=>{
+        console.log(product)
+
+        db.get().collection('product').insertOne(product).then((data)=>{
+            callback(data.insertedId)
+        })   
+    },
+    getAllProducts:()=>{
+        return new Promise(async(reslove,reject)=>{
+            let products=await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+            reslove(products)
+        })
+    }
+}
---
+++ routes/user.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+var productHelpers = require('../helpers/product-helpers');
+var userHelpers = require('../helpers/user-helpers');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  productHelpers.getAllProducts().then((products)=>{
+    console.log(products);
+    res.render('user/view-products', {products})
+  })
+});
+router.get('/login',(req, res)=>{
+  res.render('user/login')
+})
+router.post('/login',(req, res)=>{
+  userHelpers.doLogin(req.body).then((response)=>{
+    if(response.status){
+      res.redirect('/')
+    }else{
+      res.redirect('/login')
+    }
+  })
+})
+
+router.get('/signup',(req, res)=>{
+  res.render('user/signup')
+})
+router.post('/signup',(req, res)=>{
+  userHelpers.doSignup(req.body).then((response)=>{
+    console.log(response);
+  })
+
+})
+module.exports = router;
